1d1f97de5fabaf078a178bd45f5dc6fce33b891f
Alan Pason <alan.pason132@gmail.com>
2023-01-07 13:00:58 +0100

    add: selectors

---
+++ script.js
@@ -0,0 +1,3 @@
+const password = document.getElementById("pwd");
+const confirmPassword = document.getElementById("confirm-pwd");
+const requirements = document.querySelectorAll(".requirements li");
